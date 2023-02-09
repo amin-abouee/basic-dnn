15d136bd7bbc3c457d5e41cf06de29a79db1591f
Amin Abouee <amin.abouee@spleenlab.ai>
2023-02-10 00:44:21 +0100

    add adam optimizer class

---
+++ optimizers/adam.py
@@ -0,0 +1,54 @@
+import torch
+import numpy
+from typing import Tuple
+
+
+class Adam(torch.optim.Optimizer):
+    """Implements Adam algorithm.
+    It has been proposed in `Adam: A Method for Stochastic Optimization`_.
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        amsgrad (boolean, optional): whether to use the AMSGrad variant of this
+            algorithm from the paper `On the Convergence of Adam and Beyond`_
+    .. _Adam\: A Method for Stochastic Optimization:
+        https://arxiv.org/abs/1412.6980
+    .. _On the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
+    """
+
+    def __init__(self, params, lr: float = 1e-3, betas: Tuple(float, float) = (0.9, 0.999), eps: float = 1e-8,
+                 weight_decay: float = 0, optimized_update: bool = True):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay)
+        super(Adam, self).__init__(params, defaults)
+
+    def add_param_group(self):
+        pass
+
+    def load_state_dict(self):
+        pass
+
+    def state_dict(self):
+        pass
+
+    def step(self):
+        pass
+
+    def zero_grad(self):
+        pass
+
